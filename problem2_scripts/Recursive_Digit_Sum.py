97382b15e4d4fbdf9ac1c6f81c35a61501e87410
Simone Di Gregorio <63867737+sim2000dg@users.noreply.github.com>
2022-10-09 22:57:56 +0200

    Update Recursive_Digit_Sum.py

--- problem2_scripts/Recursive_Digit_Sum.py
+++ problem2_scripts/Recursive_Digit_Sum.py
@@ -1,5 +1,5 @@
 # My solution with string manipulation, but it does not pass the test cases
-# with VERY big inputs from HackerRank. Recursivity?
+# with VERY big inputs from HackerRank. Recursion?
 def superDigit(n, k):
     number = str(n)*k
     while len(number) != 1:
